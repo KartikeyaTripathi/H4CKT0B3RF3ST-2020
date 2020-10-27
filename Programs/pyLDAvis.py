7885adbccd0b3fd4e79bc7a25ac78944e89fb242
rifqi <61369248+rifqij@users.noreply.github.com>
2020-10-27 21:54:20 +0700

    Create pyLDAvis.py
    The document source is coming up next

---
+++ Programs/pyLDAvis.py
@@ -0,0 +1,17 @@
+#Visualize the topic based clustering for words in doc_term_matrix
+
+from gensim.models import ldamodel
+# Instantiate LDA model
+lda = ldamodel.LdaModel
+
+#Fit LDA model on the dataset
+num_topics=14
+%time ldamodel = lda(doc_term_matrix,num_topics=num_topics,id2word=dictionary,passes=50,minimum_probability=0)
+
+# Print the topics identified by LDA model
+ldamodel.print_topics(num_topics=num_topics)
+
+#Visualize the model
+from pyLDAvis import gensim
+lda_display = pyLDAvis.gensim.prepare(ldamodel, doc_term_matrix, dictionary, sort_topics=False, mds='mmds')
+pyLDAvis.display(lda_display)
